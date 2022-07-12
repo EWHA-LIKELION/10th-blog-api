5a4b43cdfc5e4a5f10b74d81fb1b4fd470571ce7
doyeoo <ehdusla013@gmail.com>
2022-07-12 19:17:05 +0900

    Feat signup login response message

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from .serializers import *
 from .models import *
+from django.contrib import auth
 from rest_framework import views
 from rest_framework.response import Response
 
@@ -9,34 +10,34 @@ class PostListView(views.APIView):
     def get(self, request, format=None):
         posts=Post.objects.all()
         serializer=PostSerializer(posts, many=True)
-        return Response(serializer.data)
+        return Response({'message':'게시글 목록 조회 성공', 'data':serializer.data})
     
     def post(self, request, format=None):
         serializer=PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response({'message':'게시글 작성 성공', 'data':serializer.data})
+        return Response({'message':'게시글 작성 실패', 'error':serializer.errors})
 
 
 class PostDetailView(views.APIView):
     def get(self, request, pk, format=None):
         post = get_object_or_404(Post, pk=pk)
         serializer = PostSerializer(post)
-        return Response(serializer.data)
+        return Response({'message':'게시글 상세 조회 성공', 'data':serializer.data})
 
     def put(self, request, pk, format=None):
         post = get_object_or_404(Post, pk=pk)
         serializer = PostSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response({'message':'게시글 수정 성공', 'data':serializer.data})
+        return Response({'message':'게시글 수정 실패', 'error':serializer.errors})
 
     def delete(self, request, pk, format=None):
         post = get_object_or_404(Post, pk=pk)
         post.delete()
-        return Response()
+        return Response({'message':'게시글 삭제 성공'})
 
 
 class CommentView(views.APIView):
@@ -44,5 +45,23 @@ class CommentView(views.APIView):
         serializer=CommentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response({'message':'댓글 작성 성공', 'data':serializer.data})
+        return Response({'message':'댓글 작성 실패', 'error':serializer.errors})
+
+
+class SignUpView(views.APIView):
+    def post(self, request):
+        serializer=UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message':'회원가입 성공', 'data':serializer.data})
+        return Response({'message':'회원가입 실패', 'error':serializer.errors})
+
+
+class LoginView(views.APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            return Response({"message": "로그인 성공", 'data':serializer.data})
+        return Response({"message": "로그인 실패", 'error':serializer.errors})
